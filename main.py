450f722d6f329c320f2e9eb54b006f1a1ccc0a29
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-02-15 18:27:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import schemas
 
 app = FastAPI()
 
-host_name = "54.172.221.198" # IPv4 privada de "MV Bases de Datos"
+host_name = "172.31.91.211" # IPv4 privada de "MV Bases de Datos"
 port_number = "8005"
 user_name = "root"
 password_db = "utec"
